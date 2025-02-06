011f8d6603f317205a0bcbcb4220837b4dcd7e36
ridhu-web <rkungu2@uic.edu>
2025-02-06 17:28:38 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import { useEffect, lazy, Suspense } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Routes, Route } from 'react-router-dom';
 
-import Spinner from './components/spinner/spinner.component';
-import { checkUserSession } from './store/user/user.action';
-import { GlobalStyle } from './global.styles';
-
-const Navigation = lazy(() =>
-  import('./routes/navigation/navigation.component')
-);
-const Shop = lazy(() => import('./routes/shop/shop.component'));
-const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
-const Home = lazy(() => import('./routes/home/home.component'));
-const Authentication = lazy(() =>
-  import('./routes/authentication/authentication.component')
-);
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+import { setCurrentUser } from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(checkUserSession());
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      const pickedUser =
+        user && (({ accessToken, email }) => ({ accessToken, email }))(user);
+
+      console.log(setCurrentUser(pickedUser));
+      dispatch(setCurrentUser(pickedUser));
+    });
+
+    return unsubscribe;
   }, []);
 
   return (
-    <Suspense fallback={<Spinner />}>
-      <GlobalStyle />
-      <Routes>
-        <Route path='/' element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path='shop/*' element={<Shop />} />
-          <Route path='auth' element={<Authentication />} />
-          <Route path='checkout' element={<Checkout />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
+      </Route>
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,28 +3,23 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
-import { stripePromise } from './utils/stripe/stripe.utils';
-import * as registerServiceWorker from './serviceWorkerRegistration';
+
+import './index.scss';
 
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <App />
-          </Elements>
-        </BrowserRouter>
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   rootElement
 );
-
-registerServiceWorker.register();
